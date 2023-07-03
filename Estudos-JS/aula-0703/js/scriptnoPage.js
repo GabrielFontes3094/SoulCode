42f64c262c0f521b30fcf4db4bb3db265701e22e
GabrielFontes3094 <bielfontes3094@gmail.com>
2023-07-03 12:04:37 -0300

    Estudando soulcode

---
+++ Estudos-JS/aula-0703/js/scriptnoPage.js
@@ -0,0 +1,23 @@
+console.log("texto");
+console.log(5);
+
+let nome;
+let idade;
+nome = "Gabriel Fontes";
+idade = 17;
+console.log(nome);
+console.log(idade);
+
+let cidade = "Rio Grande";
+console.log(cidade);
+let altura = 1.7;
+console.log(altura);
+let casado = false;
+console.log(casado);
+
+console.log("Nova idade:");
+idade =18;
+console.log(idade);
+
+const PI = 3.14;
+console.log(PI)
