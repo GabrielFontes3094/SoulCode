a7933d1060ea6b045a0b91d178221ab9da339a70
GabrielFontes3094 <bielfontes3094@gmail.com>
2023-07-21 14:27:32 -0300

    Estudando soulcode

---
+++ Estudos-JS/aula-0721/js/script.js
@@ -0,0 +1,11 @@
+import { melhoresFilmes } from "./filmes.js";
+function preencherLista(){
+        const lista = document.querySelector("#lista-filmes");
+        lista.innerHTML = " ";
+        melhoresFilmes.forEach(filme =>{
+            lista.innerHTML += `<li> ${filme.titulo}</li>`;
+        })
+        
+    }
+
+    preencherLista(melhoresFilmes);
